6e06db375f2a681f389398aae3e9e8d004208a85
CubeSage <79872951+CubeSage@users.noreply.github.com>
2021-05-07 13:39:25 -0600

    Add files via upload

--- TKintertest.py
+++ TKintertest.py
@@ -14,25 +14,40 @@ def exportList():
     with open("test.txt", "w") as f:
         for item in songList:
             f.write("%s\n" % item)
-    
+            
+def clearWindow():
+    for widget in top.winfo_children():
+        widget.destroy()
 
-#This is a Label widget
-L1 = Label(top, text="ourTunes")
-L1.grid(column= 0, row= 1)
+def mainMenu():
+    clearWindow()
+    LMain = Label(top, text = "Block 5 GUI Projects")
+    LMain.grid(column 0, Row 1)
+    B1Main = Button(text = , bg = "white", command = week 1)
+    B1Main.grid(column = 0, row = 1)
+    B2Main = Button(text = , bg = "light orange", 
+    B3Main
 
-#This is an Entry widget (for text entry)
-E1 = Entry(top, bd = 5)
-E1.grid(column= 0, row = 2)
+def week1():
+    clearWindow()
+    #This is a Label widget
+    L1 = Label(top, text="ourTunes")
+    L1.grid(column= 0, row= 1)
 
-#This is a Button widget
-B1 = Button(text = " + ", bg = "white", command= addTrack)
-B1.grid(column = 1, row = 2)
+    #This is an Entry widget (for text entry)
+    E1 = Entry(top, bd = 5)
+    E1.grid(column= 0, row = 2)
 
-B2 = Button(text = "Playlist", bg = "light blue", command = printList)
-B2.grid(column= 1, row = 1)
+    #This is a Button widget
+    B1 = Button(text = " + ", bg = "white", command= addTrack)
+    B1.grid(column = 1, row = 2)
 
-B3 = Button(text = "Export", bg = "#4940e6", command= exportList)
-B3.grid(column = 1, row = 3)
+    B2 = Button(text = "Playlist", bg = "light blue", command = printList)
+    B2.grid(column= 1, row = 1)
 
+    B3 = Button(text = "Export", bg = "orange", command= exportList)
+    B3.grid(column = 1, row = 3)
 
-top.mainloop()
+if _name_ == "_main_":
+    mainMenu()
+    top.mainloop()
